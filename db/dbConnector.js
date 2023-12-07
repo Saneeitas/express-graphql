3b77bdcada8ff7045e2a6a0b721cc0ee108a4094
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2023-12-07 20:41:40 +0100

    added

--- db/dbConnector.js
+++ db/dbConnector.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { environment } = require('../config/config');
-const { friendSchema } = require('./schema/friendSchema.js');
-const { seriesSchema } = require('./schema/seriesSchema.js');
+const { friendSchema } = require('../schema/friendSchema.js');
+const { seriesSchema } = require('../schema/seriesSchema.js');
 const env = process.env.NODE_ENV || "development";
 
 /**
